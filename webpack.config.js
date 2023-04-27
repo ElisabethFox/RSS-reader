b925c4c768d47a6426b376d99958d1c1c1a9cbd8
ElisabethFox <lisa.black.fox17@gmail.com>
2023-04-27 23:32:50 +0300

    fix imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,39 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
 
-const isProduction = process.env.NODE_ENV == 'production';
-
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/i,
+            loader: 'babel-loader',
+        },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-            {
-              test: /\.scss$/,
-              use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-            },
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
